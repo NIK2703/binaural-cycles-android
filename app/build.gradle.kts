835e7e2f5ecb24bb2ff44c746aa9500629be8f09
Nikita Ogurnoy <nikita.ogurnoy@mail.ru>
2026-03-12 23:28:29 +0600

    Release v0.4: нативный аудиодвижок с NEON оптимизацией

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.binauralcycles"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
